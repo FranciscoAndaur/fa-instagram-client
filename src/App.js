7fe04fc85798809aaaf362475cd29942e8edd0de
Francisco Andaur <30847974+FranciscoAndaur@users.noreply.github.com>
2021-12-01 13:57:08 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Home from "./Components/Home"
 import UploadPosts from "./Components/UploadPosts";
 import Posts from "./Containers/Posts";
@@ -7,6 +7,13 @@ function App() {
   return (
     <div>
       hi ur cute
+      <Router>
+        <Switch>
+          <Route exact path="/" component={<Home/>} ></Route>
+          <Route exact path="/UploadPosts" component={<UploadPosts/>} ></Route>
+          <Route exact path="/Posts" component={<Posts/>} ></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
